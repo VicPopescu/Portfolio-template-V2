912f79aac3e3c919b18fb8ac98e13230302b6367
Victor Popescu <nomevictor@gmail.com>
2016-07-24 17:53:59 +0300

    fixed menu color on waypoints

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,5 @@
 /* EXPERIENCE section hovering effects */
+/* Fill experience section circles with orange color when hovering */
 
 var expContent = $('.content-container');
 
@@ -14,33 +15,79 @@ $(expContent).hover(function () {
 
 }).click(function () {
     //to be added on click event
+    //    $(this).find('.details').show();
 
 });
 
+/* END */
 
-/*
-var waypoint1 = new Waypoint({
-    element: document.getElementById('homeID'),
-    handler: function () {
-        $(".top-nav ul li a").css("color", "white");
+
+/* Document waypoints */
+/* Change menu text color to blend with background */
+
+//var itemText = $(".top-nav ul li a");
+//
+//var waypoint1 = new Waypoint({
+//    element: document.getElementById('homeID'),
+//    handler: function () {
+//        $(itemText).css({
+//            "color": "white"
+//        })
+//    }
+//})
+//var waypoint2 = new Waypoint({
+//    element: document.getElementById('aboutID'),
+//    handler: function () {
+//        $(itemText).css("color", "black");
+//        $(itemHover).css("background-color", "green");
+//    }
+//})
+//var waypoint3 = new Waypoint({
+//    element: document.getElementById('portfolioID'),
+//    handler: function () {
+//        $(menu).css("color", "green");
+//    }
+//})
+//var waypoint4 = new Waypoint({
+//    element: document.getElementById('skillsID'),
+//    handler: function () {
+//        $(menu).css("color", "aqua");
+//    }
+//})
+
+var aboutScroll = $('#aboutID');
+var portfolioScroll = $('#portfolioID');
+var skillsScroll = $('#skillsID');
+var topMenuItem = $(".top-nav ul li a");
+var topMenuBottomLine = $(".nav-hr");
+var sideNavText = $('.left-side-nav-text');
+
+$(aboutScroll).waypoint(function (direction) {
+    if (direction === 'down') {
+        $(topMenuItem).css("color", "black");
+        $(topMenuBottomLine).css("border-color", "black");
+    } else {
+        $(topMenuItem).css("color", "#FFFFFF");
+        $(topMenuBottomLine).css("border-color", "#FFFFFF");
     }
-})
-var waypoint2 = new Waypoint({
-    element: document.getElementById('aboutID'),
-    handler: function () {
-        $(".top-nav ul li a").css("color", "gray");
+});
+
+$(portfolioScroll).waypoint(function (direction) {
+    if (direction === 'down') {
+        $(topMenuItem).css("color", "#FFFFFF");
+        $(topMenuBottomLine).css("border-color", "#FFFFFF");
+    } else {
+        $(topMenuItem).css("color", "black");
+        $(topMenuBottomLine).css("border-color", "black");
     }
-})
-var waypoint3 = new Waypoint({
-    element: document.getElementById('portfolioID'),
-    handler: function () {
-        $(".top-nav ul li a").css("color", "green");
+});
+
+$(skillsScroll).waypoint(function (direction) {
+    if (direction === 'down') {
+        $(topMenuItem).css("color", "black");
+        $(topMenuBottomLine).css("border-color", "black");
+    } else {
+        $(topMenuItem).css("color", "#FFFFFF");
+        $(topMenuBottomLine).css("border-color", "#FFFFFF");
     }
-})
-var waypoint4 = new Waypoint({
-    element: document.getElementById('skillsID'),
-    handler: function () {
-        $(".top-nav ul li a").css("color", "aqua");
-    }
-})
-*/
+});
