a44f192a45e3bfa81ffaab3d667fc07834056305
Victor Popescu <nomevictor@gmail.com>
2016-07-24 13:10:15 +0300

    SVG rework for IE and Moz

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,10 +1,26 @@
 /*
-var waypoint = new Waypoint({
-  element: document.getElementById('portfolioID'),
-  handler: function() {
-      alert("amma")
-    $("id").css("color", "red");
-  }
+var waypoint1 = new Waypoint({
+    element: document.getElementById('homeID'),
+    handler: function () {
+        $(".top-nav ul li a").css("color", "white");
+    }
 })
-
+var waypoint2 = new Waypoint({
+    element: document.getElementById('aboutID'),
+    handler: function () {
+        $(".top-nav ul li a").css("color", "gray");
+    }
+})
+var waypoint3 = new Waypoint({
+    element: document.getElementById('portfolioID'),
+    handler: function () {
+        $(".top-nav ul li a").css("color", "green");
+    }
+})
+var waypoint4 = new Waypoint({
+    element: document.getElementById('skillsID'),
+    handler: function () {
+        $(".top-nav ul li a").css("color", "aqua");
+    }
+})
 */
